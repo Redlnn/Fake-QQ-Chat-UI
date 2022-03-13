f0a47d3ee0e8234a106dbe34b18374d85f85e550
Redlnn <w731347477@gmail.com>
2022-03-13 21:47:47 +0800

    在Apple设备使用非衬线字体

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,8 @@
 
 <style lang="scss">
 #app {
-  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  // color: #2c3e50;
 }
 </style>
